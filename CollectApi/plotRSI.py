f7485639f05d8b7b335cafd4acf8fe63568589cf
Safa Borazan <47713659+brznk7@users.noreply.github.com>
2020-05-18 13:31:34 +0300

    Update plotRSI.py

--- CollectApi/plotRSI.py
+++ CollectApi/plotRSI.py
@@ -19,25 +19,26 @@ LowRsiLim = 20
 HighRsiLim = 80
 
 for StockName in StockNames:
-    Time = Data[StockName].index
-    Rsi = RSI(Data,StockName,Period)
-    LowRsi = np.zeros(Time.size) + LowRsiLim
-    HighRsi = np.zeros(Time.size) + HighRsiLim
-    if Rsi[-1] < 35:
-        fig, ax1 = plt.subplots()
-        ax1.plot(Time,Rsi,'red')
-        ax1.plot(Time,LowRsi,'black')
-        ax1.plot(Time,HighRsi,'black')
-        ax1.set_xlabel('time')
-        ax1.set_ylabel('RSI',color = 'red')
-        ax1.tick_params(axis='y', labelcolor='red')
-        
-        ax2 = ax1.twinx() 
-        ax2.plot(Time,Data[StockName]['Close'],color = 'blue')
-        ax2.set_ylabel('Price',color = 'blue')
-        ax2.tick_params(axis='y', labelcolor='blue')
-        plt.gcf().autofmt_xdate()
-        plt.title(StockName)
+    if StockName == 'KENT':
+        Time = Data[StockName].index
+        Rsi = RSI(Data,StockName,Period)
+        LowRsi = np.zeros(Time.size) + LowRsiLim
+        HighRsi = np.zeros(Time.size) + HighRsiLim
+        if Rsi[-1] < 100:
+            fig, ax1 = plt.subplots()
+            ax1.plot(Time,Rsi,'red')
+            ax1.plot(Time,LowRsi,'black')
+            ax1.plot(Time,HighRsi,'black')
+            ax1.set_xlabel('time')
+            ax1.set_ylabel('RSI',color = 'red')
+            ax1.tick_params(axis='y', labelcolor='red')
+            
+            ax2 = ax1.twinx() 
+            ax2.plot(Time,Data[StockName]['Close'],color = 'blue')
+            ax2.set_ylabel('Price',color = 'blue')
+            ax2.tick_params(axis='y', labelcolor='blue')
+            plt.gcf().autofmt_xdate()
+            plt.title(StockName)
 
 #from RSIgit import rsiFunc
 #RsiGit = rsiFunc(Data[StockName]['Close'],Period)
